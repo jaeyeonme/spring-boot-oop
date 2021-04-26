521a682cee14cf398757991fd63f2135d9400310
JaeYeon33 <cjyeon1022@gmail.com>
2021-04-26 23:28:12 +0900

    2021-04-26 Spring Boot (framework) Basic study file

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,18 +14,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //@Bean -> memberService -> new MemorymemberRepository()
+    //@Bean -> orderService -> new MemorymemberRepository()
+
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+    // call AppConfig.memberRepository
+
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
@@ -34,5 +50,7 @@ public class AppConfig {
         // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
+
+
 
 }
